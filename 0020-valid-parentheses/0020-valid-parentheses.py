3bb2f9d62c77963f4aac2966e1f9fc62dd1dafbb
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2023-10-08 12:22:44 +0530

    Time: 18 ms (33.38%), Space: 13.3 MB (89.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        d = {'(':')', '{':'}','[':']'}
+        stack = []
+        for i in s:
+            if i in d:  # 1
+                stack.append(i)
+            elif len(stack) == 0 or d[stack.pop()] != i:  # 2
+                return False
+        return len(stack) == 0
