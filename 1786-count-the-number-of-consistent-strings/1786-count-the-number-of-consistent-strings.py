f450ee737cd583edf85500a017bb9baca3dc1803
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2023-10-09 21:08:34 +0530

    Time: 174 ms (62.02%), Space: 15.8 MB (84.37%) - LeetHub

---
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        """
+        :type allowed: str
+        :type words: List[str]
+        :rtype: int
+        """
+        return sum(all(char in allowed for char in w) for w in words)
