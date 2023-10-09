1d19c10e67c1024bd4d96fcc06d7cd767bc8d94f
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2023-10-09 21:00:45 +0530

    Time: 21 ms (51.02%), Space: 13.5 MB (16.84%) - LeetHub

---
+++ 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def busyStudent(self, startTime, endTime, queryTime):
+        """
+        :type startTime: List[int]
+        :type endTime: List[int]
+        :type queryTime: int
+        :rtype: int
+        """
+        return sum([i <= queryTime and queryTime <= j for i, j in zip(startTime, endTime)])
