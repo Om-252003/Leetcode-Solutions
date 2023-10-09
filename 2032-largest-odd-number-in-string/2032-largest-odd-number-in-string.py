9285374721c29ac3947fbca6952acb71bf517ba0
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2023-10-09 21:21:44 +0530

    Time: 39 ms (91.84%), Space: 18.2 MB (61.63%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def largestOddNumber(self, num):
+        """
+        :type num: str
+        :rtype: str
+        """
+        for i in range(len(num) - 1, -1, -1) :
+            if num[i] in {'1','3','5','7','9'} :
+                return num[:i+1]
+        return ''
