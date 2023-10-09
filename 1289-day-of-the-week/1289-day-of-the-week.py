130558ab1e76c99899fbf39e6dbfde5661713871
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2023-10-09 20:53:43 +0530

    Time: 17 ms (26.47%), Space: 13.3 MB (23.53%) - LeetHub

---
+++ 1289-day-of-the-week/1289-day-of-the-week.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def dayOfTheWeek(self, day, month, year):
+        """
+        :type day: int
+        :type month: int
+        :type year: int
+        :rtype: str
+        """
+        return date(year, month, day).strftime("%A")
