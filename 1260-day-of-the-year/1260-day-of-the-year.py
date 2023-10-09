0e558717ff6e9baaf36c8ed4fd389d6960a2544b
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2023-10-09 20:53:13 +0530

    Time: 50 ms (65.82%), Space: 13.5 MB (27.22%) - LeetHub

---
+++ 1260-day-of-the-year/1260-day-of-the-year.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def dayOfYear(self, date):
+        """
+        :type date: str
+        :rtype: int
+        """
+        y, m, d = map(int, date.split('-'))
+        days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if (y % 400) == 0 or ((y % 4 == 0) and (y % 100 != 0)): days[1] = 29
+        return d + sum(days[:m-1])
