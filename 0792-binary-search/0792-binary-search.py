6c8c50efe985dc539c93855439063b8d1f180c5d
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-04-29 12:29:53 +0530

    Time: 0 ms (100%), Space: 13.5 MB (32.81%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -5,7 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        return self.bs(nums, 0, len(nums)-1, target)
 
-        if target in nums:
-            return nums.index(target)
-        return -1
+    def bs(self, nums, start, end, target):
+        if start > end:
+            return -1
+
+        mid = (start + end) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            return self.bs(nums, mid+1, end, target)
+        else:
+            return self.bs(nums, start, mid-1, target)
