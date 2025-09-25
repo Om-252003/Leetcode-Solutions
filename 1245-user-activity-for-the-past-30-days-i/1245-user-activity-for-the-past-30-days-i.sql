a3d03b5a53034cbd4b11cf7630a122f4f43702ae
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-09-25 10:32:40 +0530

    Time: 476 ms (62.41%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,5 @@
-select activity_date as day,
- count(distinct user_id) as active_users 
-from activity
-where (activity_date > '2019-06-27' and activity_date <= '2019-07-27') 
-group by activity_date
+select activity_date as day , count(distinct user_id) as active_users
+
+from Activity
+where activity_date Between '2019-06-28' and '2019-07-27'
+Group BY activity_date
