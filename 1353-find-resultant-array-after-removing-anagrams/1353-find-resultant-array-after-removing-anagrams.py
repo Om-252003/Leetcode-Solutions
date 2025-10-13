fee1b0305d7cd4ea995de1bddde920a87b97eb75
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-10-13 08:11:01 +0530

    Time: 2 ms (91.39%), Space: 17.6 MB (94.98%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def removeAnagrams(self, words):
-        n = len(words)
-        freq = [Counter(w) for w in words]
-        ans = [words[0]]
-        for i in range(1, n):
-            if freq[i] != freq[i - 1]:
-                ans.append(words[i])
-        return ans
+        # n = len(words)
+        # freq = [Counter(w) for w in words]
+        # ans = [words[0]]
+        # for i in range(1, n):
+        #     if freq[i] != freq[i - 1]:
+        #         ans.append(words[i])
+        # return ans
+
+        return [next(g) for _, g in groupby(words, sorted)]
