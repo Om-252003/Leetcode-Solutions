011c808dc0755d4aa8554b3fd43e77bba5a51c6a
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-10-25 09:14:19 +0530

    Time: 5 ms (6.56%), Space: 17.8 MB (37.64%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
@@ -1,16 +1,13 @@
-class Solution(object):
-    def totalMoney(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        money = 0
-        j = 1
-        for i in range(1, n+1):
-            if i % 7 == 1:
-                j = i // 7 + 1
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        k=1
+        sum = 0
+        count = 1
+        for i in range(1,n+1):
+            if (count > 7) and ((count-1) % 7 == 0):
+                k = (count//7 ) + 1
             
-            money += j
-            j += 1
-    
-        return money 
+            sum+=k
+            k+=1
+            count +=1
+        return sum
