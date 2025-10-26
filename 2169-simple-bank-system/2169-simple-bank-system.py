a9c242cd3739dea701c49b97cbc02dd34782114b
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-10-26 15:46:37 +0530

    Time: 65 ms (9.02%), Space: 47.7 MB (48.72%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.py
+++ 2169-simple-bank-system/2169-simple-bank-system.py
@@ -1,26 +1,35 @@
 class Bank:
 
     def __init__(self, balance: List[int]):
-        self.B = balance
-        self.n = len(balance)
+        self.balance = balance
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if account1>self.n or account2>self.n or self.B[account1 -1]<money:
+        if (account1 <= 0 or account1 > len(self.balance) or 
+            account2 <= 0 or account2 > len(self.balance)):
             return False
         
-        self.B[account1 - 1] -= money
-        self.B[account2 - 1] += money
+        if self.balance[account1 - 1] - money < 0:
+            return False
+        
+        self.balance[account1 - 1] -= money
+        self.balance[account2 - 1] += money
         return True
 
     def deposit(self, account: int, money: int) -> bool:
-        if account>self.n : return False
-        self.B[account-1]+=money
+        if account <= 0 or account > len(self.balance):
+            return False
+        
+        self.balance[account - 1] += money
         return True
 
     def withdraw(self, account: int, money: int) -> bool:
-        if account>self.n or self.B[account-1]<money: return False
-
-        self.B[account-1]-=money
+        if account <= 0 or account > len(self.balance):
+            return False
+        
+        if self.balance[account - 1] - money < 0:
+            return False
+        
+        self.balance[account - 1] -= money
         return True
 
 
