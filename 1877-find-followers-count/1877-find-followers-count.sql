46799ca7a4215a4ae2ae81f8c5dfd77e1959231b
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-10-26 17:22:45 +0530

    Time: 503 ms (69.7%), Space: 0B (100%) - LeetHub

--- 1877-find-followers-count/1877-find-followers-count.sql
+++ 1877-find-followers-count/1877-find-followers-count.sql
@@ -1,2 +1,4 @@
-# Write your MySQL query statement below
-select  user_id, count(user_id) followers_count  from followers group by user_id order by user_id
+select user_id, count(*) as followers_count
+from Followers
+group by user_id
+order by user_id
