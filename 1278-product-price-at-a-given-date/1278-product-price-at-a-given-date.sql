dff1f8b6e2893d1e92866408393822eb488d8c6e
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-10-28 10:01:56 +0530

    Time: 420 ms (86.95%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,12 +1,3 @@
-WITH rank_CTE AS(
-    SELECT *, RANK ()OVER(PARTITION BY product_id ORDER BY change_date desc) AS r
-FROM Products
-WHERE change_date<='2019-08-16')
-
-SELECT product_id,new_price AS price
-FROM rank_CTE
-WHERE r=1
-UNION
-SELECT product_id,10 AS price
-FROM Products
-WHERE product_id NOT IN(SELECT product_id FROM rank_CTE)
+select distinct product_id, 10 as price from Products where product_id not in(select distinct product_id from Products where change_date <='2019-08-16' )
+union 
+select product_id, new_price as price from Products where (product_id,change_date) in (select product_id , max(change_date) as date from Products where change_date <='2019-08-16' group by product_id)
