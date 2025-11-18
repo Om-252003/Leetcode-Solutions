64e86f92fcf888120f2a038ce186b0aa41d562c9
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-11-18 08:18:06 +0530

    Time: 0 ms (100%), Space: 17.8 MB (40.84%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -1,11 +1,7 @@
-class Solution(object):
-    def isOneBitCharacter(self, bits):
-        """
-        :type bits: List[int]
-        :rtype: bool
-        """
-        ret = True
-        for bit in bits[-2::-1]:
-            if bit: ret = not ret
-            else: break
-        return ret
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n = len(bits)
+        i = 0
+        while i < n - 1:
+            i += bits[i] + 1
+        return i == n - 1
