d07bac6c9ade9639fed5d4f9916925ff35bc69bf
Omsai Alladwar <147293404+Om-252003@users.noreply.github.com>
2025-11-23 21:35:12 +0530

    Time: 11 ms (91.42%), Space: 21.7 MB (66.86%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -1,11 +1,29 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        n=len(nums)
-        @cache
-        def f(i, mod):
-            if i<0: return 0 if mod==0 else -(1<<30)
-            x=nums[i]
-            modPrev=(mod-x)%3
-            if modPrev<0:modPrev+=3
-            return max(x+f(i-1, modPrev), f(i-1, mod))
-        return max(0, f(n-1, 0))
+        s = sum(nums)
+        
+        if s % 3 == 0:
+            return s
+        
+        r11 = 10000
+        r12 = 10000
+        r21 = 10000
+        r22 = 10000
+        
+        for num in nums:
+            if num % 3 == 1 and num < r12:
+                if num < r11:
+                    r12 = r11
+                    r11 = num
+                else:
+                    r12 = num
+            if num % 3 == 2 and num < r22:
+                if num < r21:
+                    r22 = r21
+                    r21 = num
+                else: 
+                    r22 = num
+        if s % 3 == 1:
+            return s - min(r11, r21+r22)
+        if s % 3 == 2:
+            return s - min(r21, r11+r12) 
